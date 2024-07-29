2ab113ed783a74109d4c9faf7ae37106f9e02f43
No-M_Duck <dimochkamikhno@gmail.com>
2024-07-29 13:58:59 +0300

    Initializing and connecting the database to the project

---
+++ src/test/java/m_duck/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package m_duck.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
